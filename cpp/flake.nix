f831a3b38ec55400ad9a13e85f2b209784d14431
constexpr-if <constexpr12@gmail.com>
2024-11-04 03:04:01 +0900

    Add template for C++

---
+++ cpp/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "C++ Template Flake which is capable of multiple packages";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      inherit (pkgs) stdenv;
+    in
+    {
+      packages.${system}.hello = stdenv.mkDerivation {
+        pname = "hello";
+        version = "0.1.0";
+        src = ./.;
+        nativeBuildInputs = with pkgs; [
+          libgcc
+          cmakeMinimal
+        ];
+        installPhase = ''
+          mkdir -p $out/lib $out/bin
+          cp -r . $out/lib
+          ln $out/lib/hello $out/bin
+        '';
+      };
+    };
+}
